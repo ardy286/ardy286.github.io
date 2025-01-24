4dcb5e3bc5e4c2930d5a4ceed73650c98d2a1cb3
2201937 <2201937@crosemont.qc.ca>
2025-01-24 17:31:37 -0500

    Ajout des autres projets.

--- javascript/script.js
+++ javascript/script.js
@@ -117,12 +117,87 @@ Cependant, la pression augmente à mesure que votre score diminue, car si vous a
       <img src="images/Position/position2.png">
       </div>
       <p text-align:justify">
-      C'est un site web de calculatrice conçu pour effectuer des calculs simples et rapides. Avec une interface claire et facile à utiliser, il permet d'effectuer des opérations de base comme l'addition, la soustraction, la multiplication et la division.
+      C’est un site web innovant qui vous permet de localiser votre position tout en fournissant des informations météorologiques précises et utiles. En quelques secondes, il détecte votre emplacement et affiche des données essentielles comme la température actuelle, le niveau d'humidité et la vitesse du vent. Ces informations sont idéales pour planifier vos activités quotidiennes ou simplement rester informé des conditions météorologiques autour de vous.
 
-Idéal pour les besoins du quotidien, ce site est accessible sur tous les appareils connectés, qu’il s’agisse d’un ordinateur, d’une tablette ou d’un téléphone portable. Sa simplicité et son efficacité en font un outil pratique pour les étudiants, les professionnels ou toute personne ayant besoin d’une calculatrice en ligne. Ce projet met l'accent sur une expérience utilisateur fluide et intuitive, avec un design épuré qui va droit au but.
+Le site est conçu avec une interface conviviale et facile à utiliser, accessible sur tous les appareils, qu’il s’agisse d’un ordinateur, d’une tablette ou d’un smartphone. Que vous soyez chez vous, en déplacement ou en voyage, vous pouvez consulter ces données en temps réel pour adapter vos décisions en fonction des conditions climatiques.
+
+En plus d’être un outil pratique, ce site met l’accent sur la précision des données, qui sont mises à jour régulièrement pour refléter les conditions les plus récentes. Il vise à simplifier l'accès aux informations météorologiques, tout en offrant une expérience utilisateur fluide et agréable. Ce projet représente une combinaison parfaite entre technologie et utilité au quotidien, rendant votre vie un peu plus facile et prévisible.
       </p>
            <h5>Lien GitHub du projet:</h5>
         <p><a text-align:justify href="https://github.com/ardy286/position" title="Cliquez pour vous rediriger sur GitHub." target="_blank" style="text-decoration:underline;color: #2EA5C3;">https://github.com/ardy286/<br>position</a></p>
+        `;
+    } else if (projectName === 'portaili') {
+        modalTitle.textContent = 'Portail informatique';
+        modalBody.innerHTML = `
+            <p>Site web développé avec SpringBoot</p>
+            <div class="imdModal">
+      <img src="images/PortailI/portaili1.png">
+      <img src="images/PortailI/portaili2.png">
+      <img src="images/PortailI/portaili3.png">
+      <img src="images/PortailI/portaili4.png">
+      </div>
+      <p text-align:justify">
+      Le portail informatique est une application conçue pour faciliter la gestion et le partage des ressources pédagogiques au sein des départements d'informatique. Cette plateforme permet aux professeurs de déposer leurs matériels, notes de cours, ainsi que divers types d’évaluations tels que des examens, travaux pratiques (TP), projets, et de partager ces ressources avec l’ensemble des enseignants du département.
+
+Pour les étudiants, l’application offre la possibilité de soumettre leurs projets de fin de session en ligne via une plateforme de dépôt comme Git ou GitLab, assurant ainsi un suivi structuré et professionnel des travaux académiques. En outre, l'application inclut un système de mise en relation qui permet aux étudiants en difficulté de trouver des tuteurs parmi leurs pairs. Ces étudiants tuteurs peuvent leur fournir une aide précieuse pour réussir leur programme en techniques d’informatique.
+
+Une autre fonctionnalité importante de l’application est l’intégration avec le milieu professionnel. Les entreprises peuvent soumettre des projets directement sur la plateforme, offrant ainsi aux étudiants des opportunités pratiques de travailler sur des cas réels tout en collaborant avec leurs enseignants. Cette dimension professionnelle enrichit la formation des étudiants, leur permettant de développer des compétences concrètes et de bâtir des expériences en lien direct avec le marché du travail.
+
+En combinant stockage centralisé des contenus, espace de collaboration interne, et ouverture vers le monde professionnel, le portail informatique devient un outil essentiel pour renforcer la synergie entre enseignants, étudiants, et entreprises.
+      </p>
+           <h5>Lien GitHub du projet:</h5>
+        <p><a text-align:justify href="https://github.com/ardy286/PortailI" title="Cliquez pour vous rediriger sur GitHub." target="_blank" style="text-decoration:underline;color: #2EA5C3;">https://github.com/ardy286/<br>PortailI</a></p>
+        `;
+    } else if (projectName === 'vinyl') {
+        modalTitle.textContent = 'Vinyl Paradise';
+        modalBody.innerHTML = `
+            <p>Application de location développé avec Kotlin sur Android Studio</p>
+            <div class="row project-card">
+      <img class="imageTlf" src="images/Vinyl/vinyl1.png">
+      <img class="imageTlf" src="images/Vinyl/vinyl2.png">
+      <img class="imageTlf" src="images/Vinyl/vinyl3.png">
+      <img class="imageTlf" src="images/Vinyl/vinyl4.png">
+      <img class="imageTlf" src="images/Vinyl/vinyl5.png">
+      </div>
+      <p text-align:justify">
+      VinylParadise est une application native développée en Kotlin, conçue pour les collectionneurs de vinyles qui souhaitent acheter et vendre leurs articles en ligne. L'application offre une expérience intuitive et conviviale, permettant aux utilisateurs de consulter un catalogue de vinyles et de publier des annonces de vente. Les principales fonctionnalités incluent la gestion des utilisateurs, une recherche avancée avec des filtres, et l'accès à l'historique des ventes. Note : Cette version ne propose pas d'intégration de système de paiement ou d'échange.
+      </p>
+           <h5>Lien GitHub du projet:</h5>
+        <p><a text-align:justify href="https://github.com/ardy286/vinyl_paradise" title="Cliquez pour vous rediriger sur GitHub." target="_blank" style="text-decoration:underline;color: #2EA5C3;">https://github.com/ardy286/<br>vinyl_paradise</a></p>
+        `;
+    }else if (projectName === 'guichet') {
+        modalTitle.textContent = 'Guichet automatique';
+        modalBody.innerHTML = `
+            <p>Application de gestion de guichet automatique d'une banque avec Kotlin sur Android Studio</p>
+            <div class="imdModal">
+      <img src="images/GuichetAutomatique/photos-a-venir-1.jpg">
+      </div>
+      <p text-align:justify">
+      Le guichet automatique est un programme conçu pour permettre aux utilisateurs d’effectuer des opérations bancaires de manière autonome, telles que retirer de l’argent, consulter leur solde, effectuer des dépôts ou transférer des fonds. Accessible 24h/24, il offre une solution pratique pour gérer ses finances sans avoir besoin de se rendre en agence bancaire.
+
+En parallèle, il y a un administrateur qui dispose de tous les droits nécessaires pour gérer le fonctionnement du guichet automatique. Il a la possibilité de superviser les transactions, de modifier les paramètres du système, d'ajouter ou de supprimer des utilisateurs, et de maintenir la sécurité du réseau. L'administrateur peut également effectuer des mises à jour logicielles ou matérielles pour garantir un service optimal, tout en assurant une gestion adéquate des comptes et de la maintenance du système. Son rôle est crucial pour le bon fonctionnement de l'infrastructure et la protection des données des utilisateurs.
+      </p>
+           <h5>Lien GitHub du projet:</h5>
+        <p style="color:gray;text-decoration:underline;">Le lien sera disponible dans pas longtemps</p>
+        `;
+    }else if (projectName === 'note') {
+        modalTitle.textContent = 'Mes Notes';
+        modalBody.innerHTML = `
+            <p>Site web développé avec PHP</p>
+            <div class="imdModal">
+      <img src="images/MyNote/photos-a-venir-1.jpg">
+      </div>
+      <p text-align:justify">
+      Mes Notes est une application pratique et performante qui offre un espace de stockage illimité pour toutes vos notes, vous permettant d’y accéder à tout moment et depuis n’importe quel endroit dans le monde. Que vous soyez à la maison, au bureau ou en déplacement, vos notes restent à portée de main, grâce à une synchronisation rapide et efficace sur tous vos appareils.
+
+L’application se distingue par son système d’organisation intelligent. Vos notes sont automatiquement classées par sections thématiques et par date, ce qui rend leur gestion simple et intuitive. Vous pouvez facilement naviguer parmi vos contenus et retrouver en quelques secondes les informations dont vous avez besoin, même dans un vaste ensemble de données.
+
+De plus, Mes Notes inclut des options de personnalisation avancées pour répondre à vos besoins spécifiques. Vous pouvez attribuer des étiquettes ou des mots-clés à vos notes pour un tri encore plus rapide, ainsi que créer des dossiers ou sous-dossiers pour structurer vos idées. L'interface conviviale, associée à des outils de recherche performants, garantit une expérience utilisateur fluide et agréable.
+
+Enfin, Mes Notes assure une sécurité optimale pour vos informations grâce à des mesures de protection robustes, comme le chiffrement des données et les sauvegardes régulières. C’est la solution idéale pour les étudiants, les professionnels ou toute personne cherchant à gérer efficacement leurs idées et projets sans craindre de perdre leurs données.
+      </p>
+           <h5>Lien GitHub du projet:</h5>
+        <p><a text-align:justify href="https://github.com/ardy286/My_note" title="Cliquez pour vous rediriger sur GitHub." target="_blank" style="text-decoration:underline;color: #2EA5C3;">https://github.com/ardy286/<br>My_note</a></p>
         `;
     } else {
         modalTitle.textContent = 'Projet inconnu';
