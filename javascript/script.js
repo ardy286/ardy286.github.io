2cabd9c829af2e4779a1b64011e7c8bde34bb54b
2201937 <2201937@crosemont.qc.ca>
2025-01-25 05:18:20 -0500

    Première amélioration d'utilisateur.

--- javascript/script.js
+++ javascript/script.js
@@ -9,10 +9,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const greetingElement = document.getElementById("greeting");
     const currentHour = new Date().getHours();
 
-    if (currentHour >= 18 || currentHour < 6) {
-        greetingElement.innerHTML = 'Bonsoir, Je suis <span class="styleLogo">A</span>riste <span class="styleLogo">K</span>indy';
+    if (currentHour >= 0 && currentHour < 12) {
+        greetingElement.innerHTML = 'Bonjour, je suis <span class="styleLogo">A</span>riste <span class="styleLogo">K</span>indy';
     } else {
-        greetingElement.innerHTML = 'Bonjour, Je suis <span class="styleLogo">A</span>riste <span class="styleLogo">K</span>indy';
+        greetingElement.innerHTML = 'Bonsoir, je suis <span class="styleLogo">A</span>riste <span class="styleLogo">K</span>indy';
     }
 });
 
